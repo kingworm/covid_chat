e58da216f8c93fed226e78ee71ba2d6903e28d49
jlucete <jlucete@gmail.com>
2020-06-04 22:13:12 +0900

    Updates

---
+++ precache-manifest.85d4cd5aca9abe5ad6219b8185e3010c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6babf83ef1c2e6f9cf325254f78c841",
+    "url": "/covid_chat/index.html"
+  },
+  {
+    "revision": "d2666497af39b96f0dc8",
+    "url": "/covid_chat/static/css/main.e364f78d.chunk.css"
+  },
+  {
+    "revision": "d0075c6f681f29c75db3",
+    "url": "/covid_chat/static/js/2.990f24a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid_chat/static/js/2.990f24a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2666497af39b96f0dc8",
+    "url": "/covid_chat/static/js/main.2ffcd9c7.chunk.js"
+  },
+  {
+    "revision": "3e42ae6fa159f4e06e1d",
+    "url": "/covid_chat/static/js/runtime-main.e04d9af2.js"
+  }
+]);
